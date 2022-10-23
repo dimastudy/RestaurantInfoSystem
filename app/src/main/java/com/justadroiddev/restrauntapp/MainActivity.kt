833c4edb80260163bbc96acce374757149c0f979
dimastudy <dimastudymail@gmail.com>
2022-10-23 20:18:52 +0300

    Created a project

---
+++ app/src/main/java/com/justadroiddev/restrauntapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.justadroiddev.restrauntapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
